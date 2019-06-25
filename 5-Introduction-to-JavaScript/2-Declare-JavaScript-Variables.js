97a0ca3245fcf3f554ce26973a911f179cb0182f
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 03:18:07 -0400

    Created a variable called  myName for lesson 2 of the Javascript module
    Also added a comment about variables in JavaScript.

---
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+// I learned how to create a variable in JavaScript with the "var" keyword. A variable allows a computer to store data.
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
