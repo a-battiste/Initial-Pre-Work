df5d77b2c7c02f05c10225864ab6daba1208ac06
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 19:51:29 -0400

    Assigned multiple lines of text to a variable with escape sequences for lesson 24 of the JavaScript module
    Also added a comment aboout escape sequences.

---
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -0,0 +1,3 @@
+// I learned how to use escape sequences in strings to type out special chracters, represent multiple quotes, and create different outputs.
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine" // Change this line;
