7f5846d3845488ccca161f1f7d9d1dc74226ae20
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 16:38:03 -0400

    Used compound assignment with division for lesson 20 of the JavaScript module
    Also added a comment about the "/=" operator.

---
+++ 5-Introduction-to-JavaScript/20-Compound-Assignment-With-Augmented-Division.js
@@ -0,0 +1,11 @@
+// I learned how to use compound assignment with divison by using the "/=" operator.
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
