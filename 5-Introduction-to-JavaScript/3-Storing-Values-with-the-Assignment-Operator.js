5021490e0325a46b758b60ac0d3f045c1fb43f44
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 03:32:18 -0400

    Created two variables to store values with the assignment operator for lesson 3 of the JavaScript module
    Also added comments about using the assignment operator.

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,9 @@
+/* I learned how to assign a value to a variable by using the assignment operator or "=" symbol. Assignnment always goes from right to left, and the value on the right has to be resolved before being assigned to a variable. */
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
