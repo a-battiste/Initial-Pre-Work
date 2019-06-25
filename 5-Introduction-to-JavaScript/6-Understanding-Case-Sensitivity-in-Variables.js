591d4a093ec363027a688c5e08fbe7bcd5f47dfe
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 04:22:00 -0400

    Changed each variable name to camelCase format for lesson 6 of the JavaScript module
    Also included a comment about variable case sensitivy and camelCase.

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,11 @@
+// I learned that variables in JavaScript are case sensitive and are usually written in camelCase. 
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
