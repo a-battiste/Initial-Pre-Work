a6fb2672e0fd0a91624669e0f0a51cfafa295bd9
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 04:54:31 -0400

    Incremented the myVar variable for lesson 11 of the JavaScript module
    Added a comment about incrementing a number in JavaScript

---
+++ 5-Introduction-to-JavaScript/11-Increment-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+// I learned how to increment a number in JavaScript by using the "++" operator. Incrementing a number adds 1 to a variable.
+
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
