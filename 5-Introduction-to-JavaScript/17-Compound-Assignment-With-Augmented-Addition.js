a4fefcb5a389bb16461ee0c56c7e918d9e5499e1
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 16:16:10 -0400

    Used compound assignment to add to variables for lesson 17 of the JavaScript module
    Added a comment about the "+=" operator.

---
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -0,0 +1,11 @@
+// I learned that you can use compound assignments to modify contents of a variable. The operator for assignment and addition is "+=".
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
