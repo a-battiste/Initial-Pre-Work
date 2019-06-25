20c32d915cd3b4add204bcb5def522c300da1ad7
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 16:48:54 -0400

    Created 2 string variables for lesson 21 of the JavaScript module
    Also added a comment explaining how to create a string variable.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// I learned how to create string variables by using a string literal, or enclosing characters in quotes. 
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Aris";
+var myLastName = "Battiste";
