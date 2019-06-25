fe830a60c5c8f88d311d5609f0f4a6ff21d3e9c2
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 15:24:16 -0400

    Initialized the myDecimal variable with a value of 5.7 for lesson 13 of the JavaScript module
    Also added a comment about creating decimal numbers in JavaScript.

---
+++ 5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+// I learned how to create a decimal number in JavaScript, and that these are also called floats or floating point numbers. 
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.7;
