69cc4d4576afe385ebbba6b9c03098c378217837
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 16:24:39 -0400

    Used compound assignment with subtraction for lesson 18 of the JavaScript module
    Added a comment about the '-=' operator.

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,11 @@
+// I learned how to use compound assignment with subtraction with the "-=" operator.
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
