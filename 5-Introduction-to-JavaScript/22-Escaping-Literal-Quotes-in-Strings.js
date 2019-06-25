f5a2d5b8aa75656a3c2a2a3a66f5e09f1dd75108
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 16:59:14 -0400

    Created a literal quote inside of a string for lesson 22 of the JavaScript module
    Also added a comment explaining how to escape literal quotes.

---
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,3 @@
+/* I learned how to escape literal quotes in a string by using \" for the beginning of the quote and \" for the end of the quote. This is done to let JavaScript know that the opening quotation mark of the literal quote isn't the end of the string. */
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
