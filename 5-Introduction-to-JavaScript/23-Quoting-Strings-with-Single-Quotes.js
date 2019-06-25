87828e5fdb17a891b7a5bbf01d5bc12c1c843ee5
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 17:35:54 -0400

    Created a string with single and double quotes for lesson 23 of the JavaScript module
    Also added a comment about using both types of quotes.

---
+++ 5-Introduction-to-JavaScript/23-Quoting-Strings-with-Single-Quotes.js
@@ -0,0 +1,3 @@
+// I learned that you can quote strings with single or double quotes. This is useful when you want to use both type of quotes in a string.
+
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
