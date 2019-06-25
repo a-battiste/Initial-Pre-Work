974bd48aeac5847fe0519ca06a15842c5f73a04c
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 05:03:01 -0400

    Decremented a number for lesson 12 of the Javascript module
    Also added a comment about decrementing numbers in JavaScript.

---
+++ 5-Introduction-to-JavaScript/12-Decrement-a-Number-with-JavaScript.js
@@ -0,0 +1,6 @@
+//I learned how to decrement a number in JavaScript by using the '--' operator. Decrementing a number decreases its value by 1.
+
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
