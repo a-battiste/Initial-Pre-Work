6ede87a0e1a2c77213441e5d5e9d9744e68cd7b0
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 03:39:32 -0400

    Initialized a variable into a value in the same line for lesson 4 of the JavaScript module
    Also added a comment about initializing variables.

---
+++ 5-Introduction-to-JavaScript/4-Initializing-Variables-with-the-Assignment-Operator.js
@@ -0,0 +1,7 @@
+// I learned that it is common practice to initialize a variable into a value in the same line that the variable is declared in. 
+
+// Example
+var ourVar = 19;
+
+// Only change code below this line
+var a = 9;
