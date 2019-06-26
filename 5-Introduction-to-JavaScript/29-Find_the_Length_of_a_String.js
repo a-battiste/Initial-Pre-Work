53142a6c9ad6d178ba4e5ed72832b587cef3bb54
Aris Battiste <aris.battiste@gmail.com>
2019-06-26 04:12:15 -0400

    Used the '.length' property for the lastNameLength variable for lesson 29 of the JavaScript module
    Added a comment explaining how to find the length of a string.

---
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,15 @@
+// I learned how to find the length of a string variable and a string literal with the '.length' property. This measures the amount of characters in a string.
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
