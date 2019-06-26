a55928e1148a0f06804bb28e6124052aaef3f1f9
Aris Battiste <aris.battiste@gmail.com>
2019-06-26 04:34:17 -0400

    Assigned a new string to the myStr variable for lesson 31 of the JavaScript module
    Added a comment about string immutability.

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// I learned that string values are immutable, which means that a string value cannot be changed other than directly changing the string literal.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
