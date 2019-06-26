d743de760285522839adb5a24379b1c65f3116ca
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 20:00:52 -0400

    Concatenated 2 strings for lesson 25 of the JavaScript module
    Added a comment about concatenating strings.

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// I learned how to concatenate strings, by using the "+" operator. Spaces between concatenated strings must be included in the strings.
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
