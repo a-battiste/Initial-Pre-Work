de089a587b9b50075d16658ad494f5eb6deac351
Aris Battiste <aris.battiste@gmail.com>
2019-06-26 05:16:09 -0400

    Found the 2nd to last character of a string for lesson 34 of the JavaScript module
    Added a comment about finding the nth to last character of a string.

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+/* I learned how to find the nth character from the last character of string. It works just like finding the last character of a string, but replace 1 with the number character away from the last character. */
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
