f77fd2d912b5a2c67b1e1733ff89a9999c4f394e
Aris Battiste <aris.battiste@gmail.com>
2019-06-25 20:18:12 -0400

    Concatenated a string with a variable for lesson 27 of the JavaScript module
    Added a comment about concatenating strings with variables.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,9 @@
+// I learned how to concatenate a string with a variable. The name of the variable in the string does not need quotes around it. 
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName ="Aris Battiste";
+var myStr = "My name is " + myName + " and I am well!";
