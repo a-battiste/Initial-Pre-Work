be99dbb1558f7ebba8af2bc55eabe7e608705bf0
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 02:07:52 -0400

    Created a return statement for the timesFive function for lesson 52 of the JavaScript module
    Added a comment what I learned from this lesson.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,14 @@
+// I learned how to return a value for a function by using a return statement.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
