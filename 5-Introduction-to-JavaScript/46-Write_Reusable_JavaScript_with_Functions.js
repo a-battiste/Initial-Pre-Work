06dbccebda754665be7c38e8e7c24a51fea8e8f1
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 00:11:26 -0400

    Created the reusableFunction function for lesson 46 of the JavaScript module
    Also added a comment about why functions are useful.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,14 @@
+//I learned how to create a function in JavaScript, which allows you to reuse code that you've created.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
