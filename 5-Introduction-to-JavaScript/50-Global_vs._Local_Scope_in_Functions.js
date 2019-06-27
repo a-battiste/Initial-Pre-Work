8d90584c7952eb33ad8a1d48a3cba1640e6d59d7
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 01:16:09 -0400

    Created a local scope variable with the same name as a global scope variable for lesson 50 of the JavaScript module
    Added a comment about local and global scope variables inside functions.

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,15 @@
+// I learned that a local variable with the same name as a global variable has precedence over a global variable inside of a function.
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
