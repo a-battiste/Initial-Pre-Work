f8210f5d80381b41a6e2ef8e5434e7748efc8b3f
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 05:14:00 -0400

    Added the strict inequality operator to an if statement for lesson 61 of the JavaScript module
    Added a comment about the !== operator.

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,17 @@
+// I learned that the strict inequality operator checks for inequality without converting the data types of each variable. This operator gives the opposite return of the the strict equality operator. 
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
