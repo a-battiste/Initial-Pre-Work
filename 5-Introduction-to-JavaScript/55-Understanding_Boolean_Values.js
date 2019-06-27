15851450b1424059f1fa42d6912973362ca21dea
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 03:54:43 -0400

    Changed the welcomeToBooleans function to return true for lesson 55 of the JavaScript module
    Added a comment about Booleans.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,10 @@
+// I learned that the Boolean is a data type the returns the value of either true or false. These values are not strings and are mutually exclusive.
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
