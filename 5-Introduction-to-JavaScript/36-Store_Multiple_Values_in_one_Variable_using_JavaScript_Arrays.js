f31d00ef7753f52bc4a9b0d84c500a6ced8a824b
Aris Battiste <aris.battiste@gmail.com>
2019-06-26 20:04:29 -0400

    Created an array variable for lesson 36 of the JavaScript module
    Also added a comment about arrays and how to create them.

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+// I learned how to create an array to store multiple pieces of data in a variable. The array uses square brackets, and a comma after each entry.
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["Aris", 25];
