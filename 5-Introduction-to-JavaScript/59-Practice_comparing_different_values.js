9d6cec8b6080f4ed7f85733b81431fd8eff334f5
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 04:56:26 -0400

    Changed the equality operator to the strict equality operator for lesson 59 of the JavaScript module
    Added a comment about the typeof operator.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+// I learned that you can use the typeof operator to determine the type of a variable or value.
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
