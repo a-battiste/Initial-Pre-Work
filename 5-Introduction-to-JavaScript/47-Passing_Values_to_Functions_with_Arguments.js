b017a3959a3a1ab58a740b26a05f23e0688c1be3
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 00:43:39 -0400

    Created a function with two arguments for lesson 47 of the JavaScript module
    Added a comment about the components of a function

---
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,14 @@
+// I learned that functions use parameters that serve as placeholders for values that will be input later, which are called arguments. 
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(a,b) {
+  console.log(a + b);
+}
+functionWithArgs(100, 50);
