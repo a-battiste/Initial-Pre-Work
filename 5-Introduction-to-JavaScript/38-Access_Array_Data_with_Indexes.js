d4196db673b30406875b40f9e93858370bd07bfc
Aris Battiste <aris.battiste@gmail.com>
2019-06-26 20:28:29 -0400

    Created the myData variable to access the first value of myArray for lesson 38 of the JavaScript module
    Added a comment about using indexes to access array data.

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+// I learned how to use indexes to access data within arrays. Array variables are also zero-indexed.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
