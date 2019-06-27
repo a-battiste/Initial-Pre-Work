57dfe0a1efe68a890039c921dd991fa8afd22a6a
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 16:48:38 -0400

    Created an else statement for a function for lesson 68 of the JavaScript module
    Added a comment about else statements.

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,20 @@
+// I learned how to create an else statement, which is another block of code to be executed if the condition for an if statement is false. 
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+
+
+  } else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
