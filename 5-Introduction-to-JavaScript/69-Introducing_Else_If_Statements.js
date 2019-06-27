0199b7aaa3ea5549bbdfe3db65673ece69487a7e
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 17:01:51 -0400

    Created an else if statement for the testElseIf function for lesson 69 of the JavaScript module
    Added a comment about else if statements.

---
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,16 @@
+// I learned how to create else if statements when there are multiple conditions that need to be addressed for a function.
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+
+
+  } else if (val < 5) {
+    return "Smaller than 5";
+
+  } else {
+  return "Between 5 and 10";
+  }
+}
+// Change this value to test
+testElseIf(7);
