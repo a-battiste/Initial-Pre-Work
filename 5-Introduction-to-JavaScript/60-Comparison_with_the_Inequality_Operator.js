8b7c3a4ee9fdb12937454e1747c8c135d214e3a7
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 05:04:59 -0400

    Added the inequality operator in the if statement for lesson 60 of the JavaScript module
    Added a comment about the inequality operator.

---
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -0,0 +1,12 @@
+// I learned that the inequality operator checks to see if two values are not equal, regardless of their types.
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
