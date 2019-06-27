a702edf261431264c31d753aa68e18a80cc1e00d
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 04:37:26 -0400

    Created a Boolean statement with the equality operator for lesson 57 of the JavaScript module
    Added a comment about the equality operator.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,12 @@
+// I learned how to use the equality operator to create a Boolean statement. The == operator can compare different data types, but converts one type to the other which is called type coercion.
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
