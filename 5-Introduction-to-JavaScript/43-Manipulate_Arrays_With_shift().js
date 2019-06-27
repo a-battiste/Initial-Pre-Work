44ab20c304fe33e28d592492bbe2ab96a042de2a
Aris Battiste <aris.battiste@gmail.com>
2019-06-26 22:11:53 -0400

    Shifted of the first item of myArray and assigned it to a new variable for lesson 43 of the JavaScript module
    Added a comment about the shift() function.

---
+++ 5-Introduction-to-JavaScript/43-Manipulate_Arrays_With_shift().js
@@ -0,0 +1,12 @@
+// I learned how to remove the first item in an array with the shift() function. The shifted off item can be assigned to a new variable.
+
+// Example
+var ourArray = ["Stimpson", "J", ["cat"]];
+var removedFromOurArray = ourArray.shift();
+// removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]].
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
