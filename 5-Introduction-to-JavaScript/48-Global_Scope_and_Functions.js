845c5e3431007090007be037debdf521e720da6c
Aris Battiste <aris.battiste@gmail.com>
2019-06-27 01:01:49 -0400

    Created a variable with global scope inside of a function for lesson 48 of the JavaScript module
    Added a comment about the importance of scope in JavaScript.

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,21 @@
+// I learned that the scope of variables is important because it can create issues in other areas of your code. A variable created outside of a function has a Global scope. Variables should always be created with the var keyword so that they are not in the Global scope.
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
