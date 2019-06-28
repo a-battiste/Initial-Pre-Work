b1b9be48a9493e9c0803dc8e51feb6bc46ad9ec6
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 03:40:30 -0400

    Changed the function to return a boolean value for lesson 77 of the JavaScript module
    Added a statement explaining how to return boolean values from a function.

---
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// I learned how to return a Boolean true or false value for a function by adding return followed by a statement under the function with an equality, strict equality. or relational operator.
+
+function isLess(a, b) {
+  // Fix this code
+ return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
