f37400faa8c8b2e72d61c55dcb3002410d222e04
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 05:27:43 -0400

    Created an object called myDog for lesson 80 of the JavaScript module
    Added a comment about objects.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,22 @@
+// I learned that objects are used to store data in a structured way, and their data is modified through their properties. Object properties are always typecast as strings.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Rex",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Loki", "Bud"],
+
+
+
+
+};
