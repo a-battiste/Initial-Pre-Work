6741a21676df01cf6046466531ea1dc07bc0784e
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 15:51:01 -0400

    Deleted the tails property from the myDog object for lesson 86 of the JavaScript module
    Added a comment about deleting properties from objects.

---
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -0,0 +1,25 @@
+// I learned that properties can be deleted from objects by using the delete keyword, followed by the object name with the property in dot or bracket notation.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+
+delete myDog.tails;
