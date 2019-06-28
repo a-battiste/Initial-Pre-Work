a13590eeae0a7f232c8c4967f74c5e8d822ab148
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 14:59:40 -0400

    Assigned testObj properties to entreeValue and drinkValue for lesson 82 of the JavaScript module
    Also added a comment about using bracket notation to access properties.

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+// I learned that bracket notation is used to access object properties when the property name has a space in it. The name must also have quotes around it within the brackets.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
