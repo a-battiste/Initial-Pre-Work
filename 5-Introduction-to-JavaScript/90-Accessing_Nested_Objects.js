e360587e8d51e49ec21e7adad1737a4fce9fbf52
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 18:10:59 -0400

    Assigned the glove box sub property to the gloveBoxContents variable for lesson 90 of the JavaScript module
    Added a comment about accessing nested objects.

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+// I learned how to access sub properties in nested objects/data structures by chaining together properties with dot and bracket notation.
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
