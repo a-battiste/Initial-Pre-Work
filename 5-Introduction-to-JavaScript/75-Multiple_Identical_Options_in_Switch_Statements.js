6e9763d8c9ad6473d93da4263584d9cfbf39d20d
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 03:09:58 -0400

    Added a switch statement to the sequentialSizes function for lesson 75 of the JavaScript module
    Added a comment about creating switch statements with multiple identical options.

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,30 @@
+// I learned how to create a switch statement with multiple options that have the same output. This is done by not adding a break or case statement for each case until the last case for that output is reached. Then the case statement is added and a break is added directly after. 
 
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      return "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      return "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return "High";
+      break;
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
