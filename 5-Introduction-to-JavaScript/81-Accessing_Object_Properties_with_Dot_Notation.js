634bc14be8c7bd60f2cb8faf70b003518a70db8b
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 14:36:17 -0400

    Assigned testObj properties to hatValue and shirtValue for lesson 81 of the JavaScript module
    Added a comma about dot notation.

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,13 @@
+// I learned how to access object properties with dot notation, which is used when you know the name of the property you want to access beforehand. 
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
