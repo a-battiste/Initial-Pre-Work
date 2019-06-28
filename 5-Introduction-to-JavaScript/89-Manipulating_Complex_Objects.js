511d5353fc8c06ffc7943c2cf1f22c1fa46af3f8
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 17:57:23 -0400

    Added another album to the myMusic array for lesson 89 of the JavaScript module
    Added a comment about complex data structures.

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,26 @@
+// I learned how to create a complex data structure by using strings, numbers, arrays, and objects. Data structures can also have booleans and functions as well. I also learned that JSON, or JavaScript Object Notation is a format to store data, as shown below. 
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  ,
+  {
+    "artist": "Travis Scott",
+    "title": "Astroworld",
+    "release_year": 2018,
+    "formats": [
+      "CD",
+      "Apple Music",
+      "LP"
+    ]
+  }
+];
