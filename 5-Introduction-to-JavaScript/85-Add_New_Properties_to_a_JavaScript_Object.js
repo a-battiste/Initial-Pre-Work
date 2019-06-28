c58af51d1a2aac6181018039c9612211cd502a65
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 15:42:22 -0400

    Added the bark property to the myDog variable for lesson 85 of the JavaScript module
    Added a comment about adding new properties to objects.

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,22 @@
+// I learned that you can add a property to an object with dot or bracket notation. 
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "woof woof";
