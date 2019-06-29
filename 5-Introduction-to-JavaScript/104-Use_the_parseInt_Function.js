f186b23b2b6a1e22c67da4b00bc46f8652e8db88
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 05:10:47 -0400

    Created a function that converts strings into numbers for lesson 104 of the JavaScript module
    Added a comment about the parseInt function.

---
+++ 5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,7 @@
+// I learned that the parseInt function converts a string into an integer. Also, if the first character in the string cannot be converted, it returns NaN.
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+
+convertToInteger("56");
