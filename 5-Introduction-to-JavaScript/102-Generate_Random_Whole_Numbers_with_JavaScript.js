6d55c73f732560674daaf8deae8d573077326034
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 04:19:08 -0400

    Created a function that creates a random whole number between 0 and 9 for lesson 102 of the JavaScript module
    Added a comment about the Math.floor function.

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,10 @@
+// I learned that the Math.floor function rounds a fractional number down to the nearest whole number.
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
