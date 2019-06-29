7af10026bf4a0fdd1f795917ae2d315696a5b92a
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 21:57:16 -0400

    Created a loop that iterates odd numbers with a for loop for lesson 95 of the JavaScript module
    Added a comment about iterating a loop with different numbers.

---
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -0,0 +1,16 @@
+// I learned how to iterate a loop by a specific number so that the values added to the array increase by a number other than 1.
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i <= 9; i+= 2) {
+  myArray.push(i);
+}
