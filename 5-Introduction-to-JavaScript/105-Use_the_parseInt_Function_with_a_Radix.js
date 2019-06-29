016b47eb95299703c83969441c47606dae42aff4
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 15:33:36 -0400

    Created a function that converts a string to a number with a radix for lesson 105 of the JavaScript module
    Added a comment about what a radix is.

---
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -0,0 +1,7 @@
+// I learned that the radix is the base of a number, which specifies the base of the string converted with the parseInt function.
+
+function convertToInteger(str, a) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
