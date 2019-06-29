5ed00870becad17d9eaf019556d7dd397f31ddef
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 21:46:29 -0400

    Created a for loop for lesson 94 of the JavaScript module
    Added a comment about for loops.

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,16 @@
+// I learned that a for loop uses three optional expressions which are separated by semicolons. The expressions are: the initialization statement, condition statement, and final expression. The initialization statement executes 1 time before the loop begins. The condition statement tells the loop to continue, as long as the condition remains true. The final expression executes at the end of each loop iteration, and usually increments or decrements the loop counter. These loops run for a specific amount of times. 
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i <= 5; i++) {
+    myArray.push(i);
+}
