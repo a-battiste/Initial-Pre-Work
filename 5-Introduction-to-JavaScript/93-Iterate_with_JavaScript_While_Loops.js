b24a211994940556c7e5ca2793e796027e76ea7a
Aris Battiste <aris.battiste@gmail.com>
2019-06-28 21:24:14 -0400

    Created a while loop that pushes numbers less than or equal to 4 to myArray for lesson 93 of the JavaScript module
    Added a comment about while loops.

---
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+// I learned how to create a while loop, which runs only while a certain condition is true. Loops run the same code multiple times.
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var i = 0;
+while(i <= 4) {
+  myArray.push(i);
+  i++;
+}
