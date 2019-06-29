1f36ee69924ea2dda378fb71958e13f04aebedc7
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 01:47:33 -0400

    Added a for loop that counts back from 9 with odd numbers for lesson 96 of the JavaScript module
    Added a comment about counting backwards with for loops.

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,16 @@
+// I learned how to create a for loop that counts backwards with odd numbers. This is similar to the last lesson, however it required a decriment of i by 2.
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i-=2) {
+myArray.push(i);
+}
