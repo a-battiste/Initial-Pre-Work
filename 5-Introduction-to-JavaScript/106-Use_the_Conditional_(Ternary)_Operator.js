5fd62d7c7573640c181a83a5333dffacd298a0bc
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 16:12:38 -0400

    Created a function that tests if two numbers are equal with a ternary operator for lesson 106 of the JavaScript module
    Added a comment about the ternary operator

---
+++ 5-Introduction-to-JavaScript/106-Use_the_Conditional_(Ternary)_Operator.js
@@ -0,0 +1,7 @@
+// I learned how to use a conditional or ternary operator, which functions similar to an if else statement. A condition is expressed and the function returns the first statement if it is true, and the second statement if it is false.
+
+function checkEqual(a, b) {
+  return a === b ? true : false;
+}
+
+checkEqual(1, 2);
