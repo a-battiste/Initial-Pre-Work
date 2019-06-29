6cefe57047cff09b6c2043ea73dddafe6437477d
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 04:11:25 -0400

    Created a function that returns a random fraction for lesson 101 of the JavaScript module
    Added a comment about the Math.random() function.

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,10 @@
+// I learned that the Math.random() function creates a random number equal to or greater than 0 but less than 1. 
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
