4d941bd31c5852a8271e1e0a64963c540432fcfe
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 18:17:16 -0400

    Created an arrow function for lesson 6 of the ES6 module
    Added a comment about arrow function uses.

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,3 @@
+// I learned that that you don't always have to name your functions, especially when the function just passed as an argument to another function. Arrow functions can simplify writing these functions by using =>. Arrow functions also serve as a way to return a return value without using the return keyword.
+
+const magic = () => new Date()
