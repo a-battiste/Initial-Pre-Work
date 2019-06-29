6e5841505367b20c86879b5b3817ec6be6e8ff91
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 17:39:03 -0400

    Created a function with const and let variables for lesson 3 of the ES6 module
    Added a comment about the const keyword.

---
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -0,0 +1,16 @@
+// I learned that the const keyword is used when you want to keep a variable constant and not allow it to be changed later within its scope.
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
