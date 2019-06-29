bd20b71b03be00c25165506f9161421dfe9e8dc3
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 16:20:15 -0400

    Created a function with multiple conditional operators for lesson 107 of the JavaScript module
    Added a comment about what I learned.

---
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -0,0 +1,7 @@
+// I learned how to chain to together multiple conditional operators to check for multiple conditions. 
+
+function checkSign(num) {
+  return num == 0 ? "zero" : num > 0 ? "positive" : "negative";
+}
+
+checkSign(10);
