845a662034f3de497fbf48b44a5b520e3496927d
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 21:55:34 -0400

    Created a function that spreads arr1 into arr2 for lesson 11 of the ES6 module
    Added a comment about the spread operator.

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,9 @@
+//I learned that the spread operator allows an array or other expression to be placed in areas where multiple parameters are expected.
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
