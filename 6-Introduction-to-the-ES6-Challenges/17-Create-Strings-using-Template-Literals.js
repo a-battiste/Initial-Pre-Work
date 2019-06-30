f693f3544ce4fda9f4930a8c1765821743749ed8
Aris Battiste <aris.battiste@gmail.com>
2019-06-30 14:51:24 -0400

    Created a string with a template literal to display the failure results for lesson 17 of the ES6 module
    Added a comment about template literals.

---
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -0,0 +1,23 @@
+// I learned how to create template literals to interpolate strings and create multi-line strings. Template literals use placeholders in the format: ${variable} and the string is wrapped in backticks.
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = arr.map(item => `<li class="text-warning">${item}</li>`);
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
