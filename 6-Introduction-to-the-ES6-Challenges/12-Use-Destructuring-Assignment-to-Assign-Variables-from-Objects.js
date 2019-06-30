6f292d16e8d546a182d5a50012807f916de32fa9
Aris Battiste <aris.battiste@gmail.com>
2019-06-30 00:58:46 -0400

    Used destructuring assignment to assign tomorrow to tempOf Tomorrow for lesson 12 of the ES6 module
    Added a comment about destructuring assignment.

---
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -0,0 +1,16 @@
+// I learned that destructuring assignment allows a property or value from an object to be assigned to a new variable.
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow : tempOfTomorrow } = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
