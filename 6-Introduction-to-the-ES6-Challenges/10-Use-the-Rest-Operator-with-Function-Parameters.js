d0fabf0cd8e75f889cf182519d1d1bdd47549001
Aris Battiste <aris.battiste@gmail.com>
2019-06-29 21:40:47 -0400

    Created a function with a rest operator for lesson 10 of the ES6 module
    Added a comment about the rest operator.

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,9 @@
+// I learned that the rest operator is used to create a more dynamic function which can be used for a various amount of arguments.
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
