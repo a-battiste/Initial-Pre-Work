e2050bd562cb1f0ddb5e733f265c49b431dba942
Aris Battiste <aris.battiste@gmail.com>
2019-06-30 23:42:31 -0400

    Imported the contents of the "capitalize_strings" file for lesson 24 of the ES6 module
    Added a comment about importing all contents of a file.

---
+++ 6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,4 @@
+// I learned that you can import all the contents of a file by using an asterisk after the import keyword, and also give the imported file a new name.
+
+"use strict";
+import * as cap_strings from "capitalize_strings";
