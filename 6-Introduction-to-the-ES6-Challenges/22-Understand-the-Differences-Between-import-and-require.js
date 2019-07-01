54c943af6704a77fc477a2a0e89ab34398b9d465
Aris Battiste <aris.battiste@gmail.com>
2019-06-30 23:11:19 -0400

    Imported the capitalizeString function from "string_functions" for lesson 22 of the ES6 module
    Added a comment about the import function.

---
+++ 6-Introduction-to-the-ES6-Challenges/22-Understand-the-Differences-Between-import-and-require.js
@@ -0,0 +1,5 @@
+// I learned that the import function is used to import a particular function from a module or file. This is useful because previously you had to use the require function which imported all functions from that source. Also, its useful to add spaces to the start and end of the imported function's name to make it easier to read. 
+
+"use strict";
+import { capitalizeString } from "string_functions"
+capitalizeString("hello!");
