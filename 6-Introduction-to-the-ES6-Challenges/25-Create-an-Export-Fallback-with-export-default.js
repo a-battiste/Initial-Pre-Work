79e8d6b5eb6cb9040ef918422255f50c1ab23a7f
Aris Battiste <aris.battiste@gmail.com>
2019-06-30 23:53:16 -0400

    Created an export fallback for lesson 25 of the ES6 module
    Added a comment about export default.

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,4 @@
+// I learned that export default is used to create a backup value for a file or value, usually only when 1 value is being exported from a file.
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
