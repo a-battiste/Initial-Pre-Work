58a8c6ed00a62057450715e16052d52f10ed4028
Aris Battiste <aris.battiste@gmail.com>
2019-06-30 23:30:00 -0400

    Exported variables foo and bar for lesson 23 of the ES6 module
    Added a comment about the export function.

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,5 @@
+// I learned how to export variables and functions by using the export function. Each variable or function can be exported individually or in one line. Exporting allows the code block to be used by another file that imports it.
+
+"use strict";
+export const foo = "bar";
+export const bar = "foo";
